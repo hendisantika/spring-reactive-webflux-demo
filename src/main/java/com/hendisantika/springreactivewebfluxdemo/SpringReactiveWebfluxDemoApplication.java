ac58affe83a9e3b22e10b62f8ba2a6cc15a087ec
hendisantika <hendisantika@yahoo.co.id>
2019-06-27 05:02:44 +0700

    #1 - Initial commit

---
+++ src/main/java/com/hendisantika/springreactivewebfluxdemo/SpringReactiveWebfluxDemoApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springreactivewebfluxdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactiveWebfluxDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringReactiveWebfluxDemoApplication.class, args);
+    }
+
+}
