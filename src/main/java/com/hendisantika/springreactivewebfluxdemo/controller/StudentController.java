8a612ab33c8fe8ded7142699ad16687948d25e6f
Hendi Santika <hendisantika@gmail.com>
2021-09-28 06:24:29 +0700

    #57 - Add student in StudentController

--- src/main/java/com/hendisantika/springreactivewebfluxdemo/controller/StudentController.java
+++ src/main/java/com/hendisantika/springreactivewebfluxdemo/controller/StudentController.java
@@ -1,9 +1,13 @@
 package com.hendisantika.springreactivewebfluxdemo.controller;
 
+import com.hendisantika.springreactivewebfluxdemo.model.Student;
 import com.hendisantika.springreactivewebfluxdemo.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -21,4 +25,10 @@ public class StudentController {
     @Autowired
     private StudentService service;
 
+    @GetMapping("/")
+    public List<Student> student() {
+        return service.getStudents();
+    }
+
 }
+
