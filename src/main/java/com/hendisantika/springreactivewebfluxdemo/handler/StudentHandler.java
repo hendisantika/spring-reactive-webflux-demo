730bc281ccf39970b228694b4a6eca2d2d32729c
Hendi Santika <hendisantika@gmail.com>
2021-09-28 06:03:50 +0700

    #47 - Add StudentHandler

---
+++ src/main/java/com/hendisantika/springreactivewebfluxdemo/handler/StudentHandler.java
@@ -0,0 +1,15 @@
+package com.hendisantika.springreactivewebfluxdemo.handler;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-reactive-webflux-demo
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 28/09/21
+ * Time: 06.03
+ */
+@Service
+public class StudentHandler {
+
+}
