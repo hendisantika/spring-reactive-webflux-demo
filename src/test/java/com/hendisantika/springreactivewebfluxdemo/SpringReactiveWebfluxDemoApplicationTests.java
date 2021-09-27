1db6c83d5d70c08f8795976abb3bce44e184b53e
Hendi Santika <hendisantika@gmail.com>
2021-09-28 06:25:38 +0700

    #59 - Add monoTest

--- src/test/java/com/hendisantika/springreactivewebfluxdemo/SpringReactiveWebfluxDemoApplicationTests.java
+++ src/test/java/com/hendisantika/springreactivewebfluxdemo/SpringReactiveWebfluxDemoApplicationTests.java
@@ -1,16 +1,16 @@
 package com.hendisantika.springreactivewebfluxdemo;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import reactor.core.publisher.Mono;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringReactiveWebfluxDemoApplicationTests {
-
     @Test
-    public void contextLoads() {
+    public void monoTest() {
+        Mono<?> justInt = Mono.just(1)
+                .then(Mono.error(new RuntimeException("Mono Exception")))
+                .log();
+        justInt.subscribe(System.out::println);
     }
-
 }
