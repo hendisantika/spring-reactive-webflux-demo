b95d7e64cc862ba644b4436f946e47ce93ddf114
Hendi Santika <hendisantika@gmail.com>
2021-09-28 06:26:07 +0700

    #60 - Add FluxTest

--- src/test/java/com/hendisantika/springreactivewebfluxdemo/SpringReactiveWebfluxDemoApplicationTests.java
+++ src/test/java/com/hendisantika/springreactivewebfluxdemo/SpringReactiveWebfluxDemoApplicationTests.java
@@ -2,6 +2,7 @@ package com.hendisantika.springreactivewebfluxdemo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @SpringBootTest
@@ -12,5 +13,13 @@ public class SpringReactiveWebfluxDemoApplicationTests {
                 .then(Mono.error(new RuntimeException("Mono Exception")))
                 .log();
         justInt.subscribe(System.out::println);
+    }
+
+    @Test
+    public void FluxTest() {
+        Flux<?> flux = Flux.just(1, 2, 3, 4)
+                .concatWith(Flux.error(new RuntimeException("Mono Exception")))
+                .log();
+        flux.subscribe(System.out::println);
     }
 }
